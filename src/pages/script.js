273da987a7b850ef699292d5338f4344ce5b05ac
Mark Kvetny <m.kvetny@outlook.com>
2018-07-10 22:02:28 +0200

    moved flickity initialization to js and added initialIndex

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,4 +1,24 @@
-const dates = document.querySelectorAll(".movie__date")
-dates.forEach(date => {
-  date.innerHTML = new Date(date.getAttribute("data-date")).toLocaleDateString("en-GB")
+const dates = []
+const dateNodes = document.querySelectorAll(".movie__date")
+dateNodes.forEach(elem => {
+  const date = new Date(elem.getAttribute("data-date"))
+  dates.push(date)
+  elem.innerHTML = date.toLocaleDateString("en-GB")
 })
+
+const flkty = new Flickity( '.carousel', {
+  initialIndex: startIndex(dates)
+});
+
+/* helper functions */
+
+function startIndex(dates) {
+  const index = dates.reduce((acc, date) => ((Date.now() - addHours(date, 20)) >= 0) ? acc += 1 : acc, 0)
+  return Math.min(index, dates.length - 1)
+}
+
+function addHours(date, h) {
+  const res = new Date()
+  res.setTime(date.getTime() + (h*60*60*1000))
+  return res
+}
