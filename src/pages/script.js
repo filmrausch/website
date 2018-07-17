d07c02d5c0eb33ff14103cd0822ff66bad78751c
Mark Kvetny <m.kvetny@outlook.com>
2018-07-17 05:15:08 +0200

    added hashes

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,6 +1,7 @@
 const flkty = new Flickity( '.carousel', {
   initialIndex: startIndex(dates.map(date => addHours(new Date(date), 20))),
-  lazyLoad: 1
+  lazyLoad: 1,
+  hash: true
 })
 
 /* helper functions */
