ac35a77ef8911878a26d0f9a980e511563abc075
Mark Kvetny <m.kvetny@outlook.com>
2018-10-02 17:36:59 +0200

    added prod build that uses the api

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,8 +4,9 @@ import del from 'del'
 import browserSync from 'browser-sync'
 import sass from 'gulp-sass'
 import babel from 'gulp-babel'
-import movies from './movies.json'
+import movieTestData from './movies.json'
 import ddmm from './util/ddmm'
+import axios from 'axios'
 
 const server = browserSync.create()
 
@@ -30,10 +31,18 @@ const paths = {
   }
 }
 
-const html = () =>
+const html = movies =>
   gulp.src(paths.html.src)
     .pipe(ejs({ ...movies, ddmm }, {}, { ext: '.html' }))
     .pipe(gulp.dest(paths.html.dest))
+
+
+const htmlProd = async () => {
+  const movies = await axios.get('http://filmrausch.hdm-stuttgart.de/api')
+  return html(movies.data)
+}
+
+const htmlDev = () => html(movieTestData)
 
 const scss = () =>
   gulp.src(paths.sass.src)
@@ -73,5 +82,6 @@ const watch = () => {
   gulp.watch(paths.assets.src, gulp.series(assets, reload))
 }
 
-gulp.task('dev', gulp.series(clean, html, scss, js, assets, serve, watch))
-gulp.task('build', gulp.series(clean, html, scss, js, assets))
+gulp.task('dev', gulp.series(clean, htmlDev, scss, js, assets, serve, watch))
+gulp.task('build', gulp.series(clean, htmlDev, scss, js, assets))
+gulp.task('build:prod', gulp.series(clean, htmlProd, scss, js, assets))
