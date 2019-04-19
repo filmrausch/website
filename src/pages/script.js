efc4cb4f98f90f9a6aaf6ea002f40190d841d783
Mark Kvetny <biowaffeln@gmail.com>
2019-04-19 18:33:53 +0200

    add faq page

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,37 +1,42 @@
-const flkty = new Flickity( '.carousel', {
+const flkty = new Flickity(".carousel", {
   initialIndex: startIndex(dates.map(date => addHours(new Date(date), 20))),
   lazyLoad: 1,
   prevNextButtons: false,
   adaptiveHeight: true,
-  pageDots: false,
-})
+  pageDots: false
+});
 
-flkty.on('select', i => {
-  document.querySelectorAll('.controls__link').forEach(e => { e.classList.remove('active') })
-  document.querySelectorAll('.controls__link')[i].classList.add('active')
-})
+flkty.on("select", i => {
+  document.querySelectorAll(".controls__link").forEach(e => {
+    e.classList.remove("active");
+  });
+  document.querySelectorAll(".controls__link")[i].classList.add("active");
+});
 
-document.getElementById('btn-prev').addEventListener('click', () => {
-  flkty.previous()
-})
-document.getElementById('btn-next').addEventListener('click', () => {
-  flkty.next()
-})
-document.querySelectorAll('.controls__link').forEach((e, i) => {
-  e.addEventListener('click', () => {
-    flkty.select(i)
-  })
-})
+document.getElementById("btn-prev").addEventListener("click", () => {
+  flkty.previous();
+});
+document.getElementById("btn-next").addEventListener("click", () => {
+  flkty.next();
+});
+document.querySelectorAll(".controls__link").forEach((e, i) => {
+  e.addEventListener("click", () => {
+    flkty.select(i);
+  });
+});
 
 /* helper functions */
 
 function startIndex(dates) {
-  const index = dates.reduce((acc, date) => ((Date.now() - date) >= 0) ? acc += 1 : acc, 0)
-  return Math.min(index, dates.length - 1)
+  const index = dates.reduce(
+    (acc, date) => (Date.now() - date >= 0 ? (acc += 1) : acc),
+    0
+  );
+  return Math.min(index, dates.length - 1);
 }
 
 function addHours(date, h) {
-  const res = new Date()
-  res.setTime(date.getTime() + (h*60*60*1000))
-  return res
+  const res = new Date();
+  res.setTime(date.getTime() + h * 60 * 60 * 1000);
+  return res;
 }
